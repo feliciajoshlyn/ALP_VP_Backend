9a58e19226f13fb728e4636cb5556c37485f83e4
Feli <feliciajoshlyn@gmail.com>
2024-12-26 21:56:28 +0700

    Update protected-router.ts

--- src/routers/protected-router.ts
+++ src/routers/protected-router.ts
@@ -1,8 +1,12 @@
 import express from "express"
 import { authMiddleWare } from "../middlewares/auth-middleware"
 import { UserController } from "../controllers/user-controller"
+import { WAMController } from "../controllers/wam-controller"
 
 export const protectedRouter = express.Router()
 protectedRouter.use(authMiddleWare)
 
 protectedRouter.delete("/api/logout", UserController.logout)
+
+protectedRouter.get("/api/wam", WAMController.getWAM)
+protectedRouter.put("/api/wam", WAMController.updateWAM)
